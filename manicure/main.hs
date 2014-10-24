ab82aacfaa71b7102dfbc3e65f7c93aec65bad2c
inkyu <gofiri@gmail.com>
2014-10-24 16:40:03 +0900

    use the ByteString package

--- manicure/main.hs
+++ manicure/main.hs
@@ -1,2 +1,20 @@
-main = do
-  putStrLn "What the hell?"
+import Data.ByteString.Char8
+ 
+import Network hiding (accept, sClose)
+import Network.Socket 
+import Network.Socket.ByteString (sendAll)
+import Control.Concurrent
+ 
+main = withSocketsDo $ do
+    sock <- listenOn $ PortNumber 5002
+    loop sock
+ 
+loop sock = do
+   (conn, _) <- accept sock
+   forkIO $ body conn
+   loop sock
+  where
+   body c = do sendAll c msg
+               sClose c
+ 
+msg = pack "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
