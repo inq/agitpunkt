62c8477f7f7e80db86cb1851d5df0f7b623751c9
inkyu <gofiri@gmail.com>
2014-10-27 09:41:33 +0900

    receive data using ByteString

--- manicure/main.hs
+++ manicure/main.hs
@@ -1,8 +1,8 @@
-import Data.ByteString.Char8
+import Data.ByteString.Char8 as BS
  
-import Network hiding (accept, sClose)
-import Network.Socket 
-import Network.Socket.ByteString (sendAll)
+import Network (PortID(PortNumber), withSocketsDo, listenOn)
+import Network.Socket (accept, sClose)
+import Network.Socket.ByteString (sendAll, recv)
 import Control.Concurrent
  
 main = withSocketsDo $ do
@@ -10,11 +10,14 @@ main = withSocketsDo $ do
     loop sock
  
 loop sock = do
-   (conn, _) <- accept sock
-   forkIO $ body conn
-   loop sock
+    (conn, _) <- accept sock
+    forkIO $ body conn
+    loop sock
   where
-   body c = do sendAll c msg
-               sClose c
+    body c = do
+        hello <- recv c 4096
+        BS.putStr hello
+        sendAll c msg
+        sClose c
  
-msg = pack "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
+msg = BS.pack "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
