1b14c77a0f2b93e2634eac21c0480027f6b7ed40
inkyu <gofiri@gmail.com>
2014-10-28 01:53:58 +0900

    use the unix socket

--- manicure/main.hs
+++ manicure/main.hs
@@ -1,12 +1,14 @@
 import Data.ByteString.Char8 as BS
  
 import Network (PortID(PortNumber), withSocketsDo, listenOn)
-import Network.Socket (accept, sClose)
+import Network.Socket (accept, bind, listen, sClose, socket, SocketType(Stream), Family(AF_UNIX), SockAddr(SockAddrUnix))
 import Network.Socket.ByteString (sendAll, recv)
 import Control.Concurrent
  
 main = withSocketsDo $ do
-    socket_fd <- listenOn $ PortNumber 5000
+    socket_fd <- socket AF_UNIX Stream 0
+    bind socket_fd $ SockAddrUnix ("manicure.sock")
+    listen socket_fd 10
     accept_socket socket_fd
  
 accept_socket socket_fd = do
