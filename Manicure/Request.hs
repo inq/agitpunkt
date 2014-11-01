4b9ef1a65ca6ac1764c294c3c98fc4cc8f5a7347
inkyu <gofiri@gmail.com>
2014-11-01 10:06:09 +0900

    parse the Method object

--- Manicure/Request.hs
+++ Manicure/Request.hs
@@ -12,11 +12,15 @@ data Request = Request {
   tmp :: [BS.ByteString]
 } deriving (Show)
 
-data Method
-  = GET
+data Method = GET
   | POST
   | PUT
   | DELETE
+  | PATCH
+  | TRACE
+  | OPTIONS
+  | HEAD
+  | CONNECT
   deriving (Show)
 
 data Version = Version {
@@ -29,14 +33,32 @@ type Header = (BS.ByteString, BS.ByteString)
 
 parse :: BS.ByteString -> Request
 parse ipt = 
-    Request GET (Version 1 1) ([(BS.pack "Hello", BS.pack "World")]) $ splitLines ipt
+    case splitLines ipt of
+        head : tail -> 
+            Request method (Version 1 1) ([(BS.pack "Hello", BS.pack "World")]) tail
+          where
+            method = parseHead head
+
+parseHead :: BS.ByteString -> Method
+parseHead str =
+    case (BS.index str 0, BS.index str 1) of
+        ('G', _) -> GET
+        ('P', 'O') -> POST
+        ('H', _) -> HEAD
+        ('P', 'U') -> PUT
+        ('D', 'E') -> DELETE
+        ('T', _) -> TRACE
+        ('C', _) -> CONNECT
+        ('O', _) -> OPTIONS
+        ('P', _) -> PATCH
+
+parseTail :: [BS.ByteString] -> [BS.ByteString]
+parseTail tail = 
+    []
 
 splitLines :: BS.ByteString -> [BS.ByteString]
-splitLines str = 
-    split [] str
-  where
-    split :: [BS.ByteString] -> BS.ByteString -> [BS.ByteString]
-    split list str = do
-        case BS.elemIndex '\r' str of
-            Just i -> split ((BS.take (i - 1) str) : list) $ BS.drop (i + 2) str
-            Nothing -> str : list
+splitLines str =
+    case BS.elemIndex '\r' str of
+        Just i | i > 2 -> BS.take i str : (splitLines $ BS.drop (i + 2) str)
+        Just i         -> [BS.drop 4 str]
+        Nothing        -> []
