08879d1f21fbabf0256aba562fa1cad95886f992
inkyu <gofiri@gmail.com>
2015-03-17 08:33:35 +0900

    implement more json specs

--- test/Manicure/JsonSpec.hs
+++ test/Manicure/JsonSpec.hs
@@ -2,6 +2,7 @@
 module Manicure.JsonSpec where
 
 import qualified Data.ByteString.Char8       as BS
+import qualified Data.Map.Strict                as M
 import Manicure.Json
 import SpecHelper
 
@@ -11,6 +12,13 @@ spec = do
     describe "Manicure.JsonSpec" $ do
         context "Simple Text" $ do
             it "parses simple string" $ do
-                 parse " \"Hello\" " `shouldBe` JSString "Hello"
+                 parse " \"Sample String\" " `shouldBe`
+                     JSString "Sample String"
+            it "parses simple array" $ do
+                 parse " [ \"1\",\"2\"\t, \"3\" ]" `shouldBe` 
+                     JSArray [JSString "1", JSString "2", JSString "3"]
+            it "parses simple object" $ do
+                 parse " { \"key\":[\"value\"] } " `shouldBe`
+                     JSObject (M.fromList [("key", JSArray [JSString "value"])])
 main :: IO ()
 main = hspec spec
