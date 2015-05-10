2984cb9094bdc635255cc812d9b7f44450f1696a
inkyu <gofiri@gmail.com>
2015-05-11 00:45:54 +0900

    escape charactor in JSON

--- src/Manicure/Json.hs
+++ src/Manicure/Json.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString.Char8          as BS
 import qualified Text.Parsec.ByteString         as PB
 import qualified Text.Parsec                    as P
 import qualified Data.Map.Strict                as M
+import Data.Char (chr)
 import Control.Applicative ((*>), (<*), (<$>), (<*>))
 import Text.Parsec ((<|>))
 import Control.Monad
@@ -29,7 +30,32 @@ parse_json :: PB.Parser Json
 -- ^ The actual parser
 parse_json = parse_object <|> parse_array <|> parse_string <|> parse_boolean <|> parse_int
   where
-    parse_string = liftM (JSString . BS.pack) $ op '"' *> (P.many . P.satisfy) (/='"') <* cl '"'
+    parse_string = liftM (JSString . BS.pack) $ op '"' *> P.manyTill any_char (P.char '"')
+      where
+        escape_map = M.fromList [
+            ('"',  '"'),
+            ('\\', '\\'),
+            ('/',  '/'),
+            ('n',  '\n'), 
+            ('r',  '\r'),
+            ('f',  '\f'),
+            ('t',  '\t'),
+            ('b',  '\b')
+          ]
+        any_char = escaped <|> P.anyChar
+        escaped = do
+            P.char '\\'
+            c <- P.anyChar
+            case M.lookup c escape_map of
+                Just ch -> return ch
+                Nothing -> if c == 'u'
+                    then do 
+                        code <- P.count 4 P.hexDigit
+                        if null code
+                             then fail "broken unicode"
+                             else return $ chr $ read $ "0x" ++ code
+                    else fail "broken escape character"
+        
     parse_array = liftM JSArray $ op '[' *> P.sepBy (P.spaces *> parse_json <* P.spaces) (P.char ',') <* cl ']'
     parse_object = liftM (JSObject . M.fromList) $ op '{' *> P.sepBy pair (P.char ',') <* cl '}'
       where
