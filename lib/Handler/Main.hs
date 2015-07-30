6782018a3a4b3d01c044e11495b525bd0b240b87
inkyu <gofiri@gmail.com>
2015-07-30 21:42:04 +0900

    decorate the index page

--- lib/Handler/Main.hs
+++ lib/Handler/Main.hs
@@ -20,6 +20,8 @@ import qualified Network.Curl                   as Curl
 import qualified Data.Map                       as M
 import qualified Models.User                    as User
 import qualified Models.Article                 as Article
+import qualified Data.Time.Format               as TF
+import qualified System.Locale                  as L
 import Control.Monad
 import Data.Map ((!))
 
@@ -73,11 +75,24 @@ index [] db req = do
   where
     read :: Bson.Document -> IO [BS.ByteString]
     read document = do
-        title   <- Mongo.lookup "title" document
-        content <- Mongo.lookup "content" document          
-        return [extract title, extract content]
+        title      <- Mongo.lookup "title" document
+        content    <- Mongo.lookup "content" document          
+        created_at <- Mongo.lookup "created_at" document
+        return [
+            extract title, 
+            extract content,
+            extract_date created_at, 
+            extract_month created_at,
+            extract_year created_at,
+            extract_time created_at
+          ]
       where
-        extract (Bson.Binary a) = a
+        extract (Bson.Bin (Bson.Binary a)) = a
+        extract_date (Bson.UTC a) = BS.pack $ TF.formatTime L.defaultTimeLocale "%d" a
+        extract_month (Bson.UTC a) = BS.pack $ TF.formatTime L.defaultTimeLocale "%b" a
+        extract_year (Bson.UTC a) = BS.pack $ TF.formatTime L.defaultTimeLocale "%Y" a
+        extract_time (Bson.UTC a) = BS.pack $ TF.formatTime L.defaultTimeLocale "%H:%M:%S" a
+
     userM = case M.lookup "SESSION_KEY" (Req.extract_cookie req) of
         Just key -> DB.run_redis db $ User.redis_get key
         Nothing  -> return Nothing
