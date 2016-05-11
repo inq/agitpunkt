afe0aca79354c5f885f44f830c2a32b57f523ab0
inkyu <gofiri@gmail.com>
2016-05-11 02:43:45 +0000

    log stdout & stderr into files

--- src/main.hs
+++ src/main.hs
@@ -4,9 +4,11 @@ import qualified Core.Launcher                  as Launcher
 
 main :: IO ()
 -- ^ The main function
-main = Launcher.daemonize pidFile $ Launcher.run Handler.routeTree databaseName socketFile
+main = Launcher.daemonize pidFile stdOut stdErr $ Launcher.run Handler.routeTree databaseName socketFile
   where
     databaseName = "manicure-test"
     socketFile = "tmp/sockets/manicure.sock"
     pidFile = "tmp/pids/manicure.pid"
+    stdOut = "tmp/logs/stdout"
+    stdErr = "tmp/logs/stderr"
 
