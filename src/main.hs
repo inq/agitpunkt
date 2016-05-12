49c2bdce7bdc7430bc5d375d1570934029f214e8
inkyu <gofiri@gmail.com>
2016-05-12 06:42:23 +0000

    pass 404 page to the manicure

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 import qualified Core.Handler                   as Handler
 import qualified Core.Launcher                  as Launcher
+import qualified Core.Html                      as Html
 
 main :: IO ()
 -- ^ The main function
-main = Launcher.daemonize pidFile stdOut stdErr $ Launcher.run Handler.routeTree databaseName socketFile
+main = Launcher.daemonize pidFile stdOut stdErr process
   where
+    process = Launcher.run Handler.routeTree response404 databaseName socketFile
+    response404 = $(Html.parseFile "404.html.qh")
     databaseName = "manicure-test"
     socketFile = "tmp/sockets/manicure.sock"
     pidFile = "tmp/pids/manicure.pid"
