35201f8569c98ea3126507c593e520c3e8e88038
inkyu <gofiri@gmail.com>
2016-10-16 09:59:35 +0800

    fix hspec

--- spec/Misc/JsonSpec.hs
+++ spec/Misc/JsonSpec.hs
@@ -1,24 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Core.JsonSpec where
+module Misc.JsonSpec where
 
 import qualified Data.Map.Strict                as M
-import Core.Json
+import Misc.Json
 import SpecHelper
 
 spec :: Spec
-spec = 
-  describe "Core.JsonSpec" $ 
+spec =
+  describe "Misc.JsonSpec" $
     context "Simple Text" $ do
-      it "parses simple string" $ 
+      it "parses simple string" $
         parse " \"Sample String\" " `shouldBe`
           JSString "Sample String"
-      it "parses simple array" $ 
-        parse " [ \"1\",\"2\"\t, \"3\" ]" `shouldBe` 
+      it "parses simple array" $
+        parse " [ \"1\",\"2\"\t, \"3\" ]" `shouldBe`
           JSArray [JSString "1", JSString "2", JSString "3"]
-      it "parses simple object" $ 
+      it "parses simple object" $
         parse " { \"key\":[\"value\"] } " `shouldBe`
           JSObject (M.fromList [("key", JSArray [JSString "value"])])
-      it "parses quoted string" $ 
+      it "parses quoted string" $
         parse " \" \\\" Hello \" " `shouldBe`
           JSString " \" Hello "
 
