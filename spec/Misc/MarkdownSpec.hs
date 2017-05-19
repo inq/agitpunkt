760c11c3b61ca353fdc0de9a089a6448f70c8db0
inkyu <gofiri@gmail.com>
2017-05-19 19:15:50 +0900

    wrap multiple images

--- spec/Misc/MarkdownSpec.hs
+++ spec/Misc/MarkdownSpec.hs
@@ -12,18 +12,22 @@ spec =
       it "parses simple string" $ do
         parse "#### Hello" `shouldBe` Just (Markdown [H4 "Hello"])
         parse "! hi-hihi; /img/img.png\r\n" `shouldBe`
-          Just (Markdown [Img "hi-hihi" "/img/img.png"])
+          Just (Markdown [Container [Img "hi-hihi" "/img/img.png"]])
         parse "Hello\r\n\r\nHihi\r\n" `shouldBe`
           Just (Markdown [Paragraph "Hello", Paragraph "Hihi"])
       it "parses simple enippet" $
         parse "Hello\r\n```rust\r\nHELLO\r\nWorld\r\n```\r\n" `shouldBe`
-        Just (Markdown [Paragraph "Hello", Snippet "rust" ["HELLO", "World"]])
+          Just (Markdown [Paragraph "Hello", Snippet "rust" ["HELLO", "World"]])
+    context "Multiple Images" $ do
+      it "renders multiple images" $ do
+        parse "{{{\r\n! hi-hihi; /img/img.png\r\n! hi-hihi; /img/img.png\r\n! hi-hihi; /img/img.png\r\n}}}\r\n" `shouldBe`
+          Just (Markdown [Container [Img "hi-hihi" "/img/img.png", Img "hi-hihi" "/img/img.png", Img "hi-hihi" "/img/img.png"]])
     context "To Html" $ do
       it "generate h4 string" $
         toHtml (Markdown [Paragraph "HI"]) `shouldBe` "<p>HI</p>"
       it "generate img tag" $
-        toHtml (Markdown [Img "hi-hi" "/img/img.png"]) `shouldBe`
-        "<img class='content-img' alt='hi-hi' src='/img/img.png'>"
+        toHtml (Markdown [Container [Img "hi-hi" "/img/img.png"]]) `shouldBe`
+        "<div class='paragraph1'><img class='content-img' alt='hi-hi' src='/img/img.png'></div>"
       it "generate paragraphs" $
         toHtml (Markdown [Paragraph "HI", Paragraph "Hello"]) `shouldBe`
         "<p>HI</p><p>Hello</p>"
--- src/Misc/Markdown.hs
+++ src/Misc/Markdown.hs
@@ -14,14 +14,18 @@ newtype Markdown =
   Markdown [Item]
   deriving (Eq, Show)
 
+data Img
+  = Img !L.Text
+        !L.Text
+  deriving (Eq, Show)
+
 data Item
   = H5 !L.Text
   | H4 !L.Text
   | H3 !L.Text
   | H2 !L.Text
   | H1 !L.Text
-  | Img !L.Text
-        !L.Text
+  | Container [Img]
   | Quote !L.Text
   | Paragraph !L.Text
   | Snippet !L.Text
@@ -39,27 +43,46 @@ parse str =
 parseItem :: P.Parser Item
 -- ^ The subparser
 parseItem =
-  parseHeader <|> parseSnippet <|> parseQuote <|> parseImage <|> parseParagraph
+  parseHeader <|> parseContainer <|> parseSnippet <|> parseQuote <|> parseImage <|> parseParagraph
   where
-    parseEnd = do
-      _ <- P.try (P.string "```")
-      return []
-    parseLine =
-      parseEnd <|>
-      (((:) . L.fromStrict) <$> (P.noneOf "\r\n" <* P.string "\r\n") <*> parseLine)
+    parseContainer = do
+    -- ^ Contain the img tags.
+      open <-
+        L.fromStrict <$>
+        P.try (P.string "{{{" <* P.string "\r\n")
+      inner <- parseLine
+      return $ Container inner
+      where
+      parseLine =
+        parseEnd <|>
+        ((:) <$> parseImg <*> parseLine)
+      parseEnd = do
+        _ <- P.try (P.string "}}}")
+        return []
     parseSnippet = do
       open <-
         L.fromStrict <$>
         P.try (P.string "```" *> P.noneOf1 "\r\n" <* P.string "\r\n")
       res <- (map stripHtml) <$> parseLine
       return $ Snippet open res
-    parseImage = do
+      where
+      parseLine =
+        parseEnd <|>
+        (((:) . L.fromStrict) <$> (P.noneOf "\r\n" <* P.string "\r\n") <*> parseLine)
+      parseEnd = do
+        _ <- P.try (P.string "```")
+        return []
+    parseImg = do
+    -- ^ Img tag. It must be wrapped by Container.
       alt <-
         L.fromStrict <$>
         P.try
           (P.char '!' *> P.spaces *> P.noneOf1 ";" <* P.char ';' <* P.spaces)
       uri <- L.fromStrict <$> P.noneOf1 "\r" <* P.string "\r\n"
       return $ Img alt uri
+    parseImage = do
+      img <- parseImg
+      return $ Container [ img ]
     parseHeader = do
       sharps <- P.try (P.many1 (P.char '#')) <* P.spaces
       rest <- L.fromStrict <$> P.noneOf1 "\r\n"
@@ -97,8 +120,13 @@ toStr (H4 str) = L.concat ["<h4>", str, "</h4>"]
 toStr (H3 str) = L.concat ["<h3>", str, "</h3>"]
 toStr (H2 str) = L.concat ["<h2>", str, "</h2>"]
 toStr (H1 str) = L.concat ["<h1>", str, "</h1>"]
-toStr (Img alt uri) =
-  L.concat ["<img class='content-img' alt='", alt, "' src='", uri, "'>"]
+toStr (Container imgs) =
+  L.concat (["<div class='paragraph", (L.pack $ show $ length imgs), "'>"]
+  ++ renderImgs imgs ++ ["</div>"])
+  where
+  renderImgs (Img alt uri : t) =
+    L.concat ["<img class='content-img' alt='", alt, "' src='", uri, "'>"] : renderImgs t
+  renderImgs [] = []
 toStr (Quote str) = L.concat ["<blockquote><p>", str, "</p></blockquote>"]
 toStr (Paragraph str) = L.concat ["<p>", str, "</p>"]
 toStr (Snippet _ strs) =
